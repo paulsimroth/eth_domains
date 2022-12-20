b0833464998e9378a8c773fb03fe75306fd0813b
Paul Simroth <paul.simroth@proton.me>
2022-12-20 13:33:35 +0100

    website initial steps

--- src/App.js
+++ src/App.js
@@ -14,12 +14,25 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({"method": "eth_requestAccounts"});
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account)
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
     <div>
 
       <div className='cards__section'>
 
         <h2 className='cards__title'>Welcome to ETH Domains!</h2>
+        <p>{account}</p>
 
       </div>
 
