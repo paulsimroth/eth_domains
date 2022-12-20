156e9156700bd16926bf7bc2bf03a36badda636f
Paul Simroth <paul.simroth@proton.me>
2022-12-20 10:51:09 +0100

    constructor and tests

--- contracts/ETHDomains.sol
+++ contracts/ETHDomains.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract ETHDomains {
-    string public name = "ETH Domains";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ETHDomains is ERC721 {
+    
+    address public owner;
+
+    constructor(string memory _name, string memory _symbol) 
+        ERC721(_name, _symbol)
+    {
+        owner = msg.sender;
+    }
 }
--- test/ETHDomains.js
+++ test/ETHDomains.js
@@ -1,16 +1,43 @@
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+};
 
 describe("ETHDomains", () => {
+  let ethDomains;
+  let deployer, owner1;
 
-  it("has a name", async() => {
+  const NAME = "ETH Domains";
+  const SYMBOL = "ETHD";
+
+  beforeEach(async () => {
+    //Set up accounts
+    [deployer, owner1] = await ethers.getSigners();
+
+    //Deploy Contract
     const ETHDomains = await ethers.getContractFactory("ETHDomains");
-    let ethDomains = await ETHDomains.deploy();
-    const result = await ethDomains.name();
-    expect(result).to.equal("ETH Domains");
-  })
+    ethDomains = await ETHDomains.deploy(NAME, SYMBOL);
+  });
+
+  describe("Deployment", () => {
+
+    it("has a name", async() => {
+      const result = await ethDomains.name();
+      expect(result).to.equal("ETH Domains");
+    })
+
+    it("has a symbol", async() => {
+      const result = await ethDomains.symbol();
+      expect(result).to.equal("ETHD");
+    })
+
+    it("sets owner", async() => {
+      const result = await ethDomains.owner();
+      expect(result).to.equal(deployer.address);
+    })
+
+  });
+
 })
