a5a18ed4f644cc484ada4340712129cd308b8aa7
Paul Simroth <paul.simroth@proton.me>
2022-12-20 12:15:07 +0100

    deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 const tokens = (n) => {
@@ -11,7 +12,28 @@ const tokens = (n) => {
 }
 
 async function main() {
+  //Accounts and Variables
+  const [deployer] = await ethers.getSigners();
+  const NAME = "ETH Domains";
+  const SYMBOL = "ETHD";
 
+  //Deploy contract
+  const ETHDomains = await ethers.getContractFactory("ETHDomains");    
+  const ethDomains = await ETHDomains.deploy(NAME, SYMBOL);
+  await ethDomains.deployed();
+
+  console.log(`Deployed Domain Contract at: ${ethDomains.address} \n`);
+  
+  // List 6 domains
+  const names = ["jack.eth", "paul.eth", "henry.eth", "michael.eth", "oxygen.eth", "raphael.eth"];
+  const costs = [tokens(10), tokens(25), tokens(15), tokens(2.5), tokens(3), tokens(1)];
+
+  for (var i = 0; i < 6; i++) {
+    const transaction = await ethDomains.connect(deployer).list(names[i], costs[i]);
+    await transaction.wait();
+
+    console.log(`Listed Domain ${i + 1}: ${names[i]}`);
+  }
 }
 
 main().catch((error) => {
